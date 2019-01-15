2d0a7b891f6ad9875e663403cd5944fbde3d6daa
lindroos <emptysmile1013@gmail.com>
2019-01-15 10:29:16 +0800

    copy existing lines below .zshrc others comments into new .zshrc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 cp ./tmux.conf ~/.tmux.conf
 cp ./vimrc ~/.vimrc
-cp ./zshrc ~/.zshrc
+
+if [ -z "$(grep -n 'others' ~/.zshrc | cut -d: -f 1)" ]; then
+    cp ./zshrc ~/.zshrc
+else
+    cp ./zshrc ~/.zshrc.tmp
+    tail -n +"$(grep -n 'others' ~/.zshrc | cut -d: -f 1)" ~/.zshrc >> ~/.zshrc.tmp
+    mv ~/.zshrc.tmp ~/.zshrc
+fi
