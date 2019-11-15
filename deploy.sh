0ae97c27c1b4339edf4bbc2d35294ececb9b7304
tommady <tommady@users.noreply.github.com>
2019-11-15 20:29:37 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 cp ./tmux.conf ~/.tmux.conf
 cp ./init.vim ~/.config/nvim/init.vim
+cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ./p10k.zsh ~/.p10k.zsh
 
+# prevent target host does not have rg, so using grep here
 if [ -z "$(grep -n 'others' ~/.zshrc | cut -d: -f 1)" ]; then
     cp ./zshrc ~/.zshrc
 else
