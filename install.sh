7c5d9a8f15582ddecc918910e00c505c9a142dd9
tommady <tommady@users.noreply.github.com>
2021-12-19 21:46:33 +0800

    install.sh for testing

--- install.sh
+++ install.sh
@@ -1,1 +1,79 @@
-echo "hehe testing der lah"
+#!/bin/sh
+
+main() {
+    need_cmd uname
+    need_cmd mktemp
+    need_cmd rmdir
+    need_cmd git
+    need_cmd make
+
+    check_architecture
+    ensure install_nerd_font_SauceCodePro
+    ensure install_oh_my_zsh
+    ensure install_powerlevel10k
+    ensure download_scripts
+    ensure deploy_scripts
+}
+
+function deploy_scripts() {
+    cd $HOME/tfiles
+    make
+}
+
+function download_scripts() {
+    git clone --depth 1 https://github.com/tommady/tfiles.git $HOME/tfiles
+}
+
+function install_powerlevel10k() {
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    # p10k configure <--- run it if you don't like my configuration of p10k
+}
+
+function install_oh_my_zsh() {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+function install_nerd_font_SauceCodePro {
+    local _dir="$(mktemp -d)"
+    local _file="${_dir}/nerd-fonts"
+    git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts $_file
+    cd "$_file"
+    git sparse-checkout add patched-fonts/SauceCodePro
+    ./install.sh SauceCodePro
+
+    ignore rmdir "$_dir"
+}
+
+function check_architecture() {
+    local _ostype
+    _ostype="$(uname -s)"
+
+    if ![[ "$_ostype" = Linux || "$_ostype" = Darwin ]]; then
+        err "only support Linux or Darwin architectures"
+    fi
+}
+
+ensure() {
+    if ! "$@"; then err "command failed: $*"; fi
+}
+
+ignore() {
+    "$@"
+}
+
+need_cmd() {
+    if ! check_cmd "$1"; then
+        err "need '$1' (command not found)"
+    fi
+}
+
+say() {
+    printf 'tfile: %s\n' "$1"
+}
+
+err() {
+    say "$1" >&2
+    exit 1
+}
+
+main "$@" || exit 1main "$@" || exit 1
